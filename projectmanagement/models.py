95cfa4a4d5b40bc89a3de7c7cf165a61b283fc19
j.volland <volland.jimmy@gmail.com>
2024-03-12 11:00:38 +0100

    définition des entitées

--- projectmanagement/models.py
+++ projectmanagement/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
+
 # Create your models here.
+class Projet(models.Model):
+    nom = models.CharField(max_length=40)
+    statut = models.IntegerField()
+    dateLivraison = models.DateField()
+    responsable = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
+    dateDebut = models.DateField()
+    avancementEffectif = models.FloatField()
+    avancementSuppose = models.FloatField()
+
+    def __str__(self):
+        return self.nom
+
+
+class Tache(models.Model):
+    nom = models.CharField(max_length=200)
+    description = models.TextField()
+    projet = models.ForeignKey(Projet, on_delete=models.CASCADE)
+    priorite = models.IntegerField()
+    dateDebut = models.DateField()
+    dateFin = models.DateField()
+    statut = models.IntegerField()
+    executant = models.ManyToManyField(Utilisateur)
+    tacheParent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.nom
+
+
+class Utilisateur(models.Model):
+    nom = models.CharField(max_length=200)
+    mdp = models.TextField(max_length=255)
+    mail = models.EmailField()
+    estResponsable = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nom
+
+
+class FormulaireTache(forms.ModelForm):
+    class Meta:
+        model = Tache
+        fields = ['nom', 'description', 'projet', 'priorite', 'dateDebut', 'dateFin', 'statut', 'executant',
+                  'parent']
