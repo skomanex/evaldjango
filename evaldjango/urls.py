b1851061347ad84a166edf8811803fc0c742e451
j.volland <volland.jimmy@gmail.com>
2024-03-14 10:12:53 +0100

    correction models

--- evaldjango/urls.py
+++ evaldjango/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = {
     path('admin/', admin.site.urls),
-    path('projectmanagement', include('projectmanagement.urls')),
+    path('projectmanagement/', include('projectmanagement.urls')),
 }
