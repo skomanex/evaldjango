6371b9a6c76f8acfe9ee37609a2598cc7fd0a6b4
j.volland <volland.jimmy@gmail.com>
2024-03-14 09:18:49 +0100

    correction models

--- projectmanagement/models.py
+++ projectmanagement/models.py
@@ -47,3 +47,8 @@ class FormulaireTache(ModelForm):
         model = Tache
         fields = ['nom', 'description', 'projet', 'priorite', 'dateDebut', 'dateFin', 'statut', 'executant',
                   'tacheParent', 'rapportAvancement', 'avancement']
+
+class FormulaireProjet(ModelForm):
+    class Meta:
+        model = Projet
+        fields = ['nom', 'statut', 'dateLivraison', 'responsable', 'dateDebut']
