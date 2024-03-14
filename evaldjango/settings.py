e5f7e4ff3e0707bbf65fe35cb8350595c145b91d
fpiuzzi <147161762+fpiuzzi@users.noreply.github.com>
2024-03-14 09:51:16 +0100

    Add url

--- evaldjango/settings.py
+++ evaldjango/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qd_(d+d0m=*0i$5d9k69@82!qi3bvd5r32#5c&qpa^(vjq%$4@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["141.94.237.180", "192.168.56.1", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["141.94.237.180", "192.168.56.12", "127.0.0.1", "localhost"]
 
 # Application definition
 
--- projectmanagement/models.py
+++ projectmanagement/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.forms import ModelForm
 
+
 # Create your models here.
 class Utilisateur(models.Model):
     nom = models.CharField(max_length=200)
