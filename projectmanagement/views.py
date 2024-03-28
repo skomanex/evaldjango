416626662500485b5ea357885881a20e735e7fa8
skomanex <volland.jimmy@gmail.com>
2024-03-28 06:23:58 -0400

    ajout d'une session de login, la page utilisateur n'est maintenant accessible que depuis la vue login.

--- projectmanagement/views.py
+++ projectmanagement/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from projectmanagement.models import *
@@ -10,33 +10,43 @@ def login(request):
         password = request.POST.get('password')
         try:
             user = Utilisateur.objects.get(mail = email, mdp = password)
+
+            # Création de la session de login
+            request.session['pp_login'] = True
             return redirect(reverse('pageutilisateur', args = [user.nom]))
         except Utilisateur.DoesNotExist:
             return render(request, 'login.html', {'error': 'Mauvais mail ou mot de passe'})             
     return render(request, "login.html")
 
 def pageutilisateur(request, utilisateur):
-    try: 
-        user = Utilisateur.objects.get(nom = utilisateur)
-        projects = Projet.objects.filter(responsable__id = user.id)
-        tasks = Tache.objects.filter(executant__id = user.id)
+    if 'pp_login' in request.session:
+        try: 
+            user = Utilisateur.objects.get(nom = utilisateur)
+            projects = Projet.objects.filter(responsable__id = user.id)
+            tasks = Tache.objects.filter(executant__id = user.id)
 
-        for project in projects:
-            try:
-                earliest = Tache.objects.filter(projet_id = project.id).order_by('dateDebut')[0]
-                project.dateDebut = earliest.dateDebut
-                latest = Tache.objects.filter(projet_id = project.id).order_by('-dateFin')[0]
-                project.dateLivraison = latest.dateFin
-            except IndexError:
-                project.dateDebut = None
-                project.dateLivraison = None
+            for project in projects:
+                try:
+                    earliest = Tache.objects.filter(projet_id = project.id).order_by('dateDebut')[0]
+                    project.dateDebut = earliest.dateDebut
+                    latest = Tache.objects.filter(projet_id = project.id).order_by('-dateFin')[0]
+                    project.dateLivraison = latest.dateFin
+                except IndexError:
+                    project.dateDebut = None
+                    project.dateLivraison = None
 
-        data = {
-            'utilisateur': user.to_json(),
-            'projects': [project.to_json() for project in projects],
-            'tasks': [task.to_json() for task in tasks]
-        }
-        return render(request, "pageutilisateur.html", data)
-    
-    except Utilisateur.DoesNotExist:
-        return render(request, "pageutilisateur.html", {'error' : 'Utilisateur introuvable'})
+            data = {
+                'utilisateur': user.to_json(),
+                'projects': [project.to_json() for project in projects],
+                'tasks': [task.to_json() for task in tasks]
+            }
+
+            # Suppression de la session de login
+            del request.session['pp_login']
+            return render(request, "pageutilisateur.html", data)
+        
+        # Artefact datant d'avant le login, puisque le login vérifie déjà l'existance en BDD de l'utilisateur le try except est redondant.
+        except Utilisateur.DoesNotExist:
+            return render(request, "pageutilisateur.html", {'error' : 'Utilisateur introuvable'})
+    else :
+        raise Http404
